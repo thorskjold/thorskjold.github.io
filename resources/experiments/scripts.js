5ece66c16344d38d92e3d0203aafcefe1fd03bdd
Thor <thorlindberg@icloud.com>
2021-03-04 18:32:09 +0100

    Condensed experiment JS

--- resources/experiments/scripts.js
+++ resources/experiments/scripts.js
@@ -1,3 +1,35 @@
+/* experiment 1 */
+
+function setBackground(colors, delay) {
+
+    window.audio.pause();
+    clearInterval(window.running);
+
+    var iteration = 0;
+    
+    window.running = setInterval(function() {
+
+        if (iteration < colors.length - 1) {
+            document.body.style.background = "rgb(" + String(colors[iteration]) + ")";
+            iteration++;
+        } else {
+        	document.body.style.background = "rgb(" + String(colors[iteration]) + ")";
+            iteration = 0;
+        };
+
+    }, delay);
+
+};
+
+function play() {
+
+    window.audio = document.getElementById("audio");
+    window.audio.play();
+
+};
+
+/* experiment 2 */
+
 function mouseBackground(event) {
 
     let xPos = event.clientX;
