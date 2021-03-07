d23aebb15350b20d578327966ecc6e49f4df8dd5
Thor <thorlindberg@icloud.com>
2021-03-07 14:56:22 +0100

    Moved preload from scripts.js to avoid attempted preload on every page

--- scripts.js
+++ scripts.js
@@ -5,37 +5,3 @@ if (history.scrollRestoration) {
         window.scrollTo(0, 0);
     };
 };
-
-window.addEventListener('load', function () {
-
-    var element = document.getElementById("index");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view1");
-    element.classList.add("fadeZoomUp");
-    
-    var element = document.getElementById("view2");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view3");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view4");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view5");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view6");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view7");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view8");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view9");
-    element.classList.add("fadeZoomUp");
-
-});
