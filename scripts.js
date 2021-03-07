4e513ed6c40d994de904fb83e39d4ce999a6fa01
Thor <thorlindberg@icloud.com>
2021-03-07 15:15:45 +0100

    Attempt to animate preload of non-index pages

--- scripts.js
+++ scripts.js
@@ -9,36 +9,6 @@ if (history.scrollRestoration) {
 window.addEventListener('load', function () {
 
     var element = document.getElementById("page");
-    element.style.opacity = "1";
-
-    var element = document.getElementById("index");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view1");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view2");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view3");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view4");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view5");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view6");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view7");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view8");
-    element.classList.add("fadeZoomUp");
-
-    var element = document.getElementById("view9");
-    element.classList.add("fadeZoomUp");
+    element.classList.add("fadeIn");
 
 });
