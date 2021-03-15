ab6edb92e9745d3d80ffcb89a48a70474dfc96a7
Thor <thorlindberg@icloud.com>
2021-03-15 20:21:14 +0100

    Attempt at correcting loading animation

--- scripts.js
+++ scripts.js
@@ -123,8 +123,8 @@ document.querySelectorAll(".sticky").forEach((sticky) => {
 window.addEventListener('load', function () {
 
   if (parent.document.getElementById("loading") != null) {
-    parent.document.getElementById("loading").style.animation = "spin 1.5s linear infinite, fadeOut 1s";
-    setTimeout(function() { parent.document.getElementById("loading").style.display = "none"; }, 1000);
+    parent.document.getElementById("loading").style.animation = "spin 1.5s linear infinite, out 0.5s";
+    setTimeout(function() { parent.document.getElementById("loading").style.display = "none"; }, 500);
   };
 
   if (document.getElementById("page") != null) {
@@ -140,7 +140,8 @@ window.destination = 'home';
 function navigate(id) {
 
   if (id != parent.window.destination) {
-    parent.document.getElementById("loading").style.display = "block";
+    parent.document.getElementById("loading").style.animation = "spin 1.5s linear infinite, in 0.5s";
+    setTimeout(function() { parent.document.getElementById("loading").style.display = "block"; }, 500);
     parent.document.getElementById(window.destination).classList.remove('selected');
     parent.document.getElementById(id).classList.add('selected');
     parent.document.getElementById('destination').src = 'pages/' + id + '.html';
