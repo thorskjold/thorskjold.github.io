c959e4c3fe482a17c1d6ae9e24bccb53180820dd
Thor <thorlindberg@icloud.com>
2021-03-15 19:27:14 +0100

    Added cursor stickyness. Experiments now use dir JS

--- scripts.js
+++ scripts.js
@@ -32,6 +32,8 @@ document.addEventListener("mouseout", () => {
   }
 });
 
+/* cursor text-selection */
+
 document.querySelectorAll(".select").forEach((select) => {
   select.addEventListener(
     "mouseover",
@@ -47,6 +49,70 @@ document.querySelectorAll(".select").forEach((select) => {
     () => {
       cursor.style.setProperty("--width", DEFAULT_CURSOR_SIZE);
       cursor.style.setProperty("--height", DEFAULT_CURSOR_SIZE);
+    },
+    { passive: true }
+  );
+});
+
+/* cursor sticky */
+
+document.querySelectorAll(".sticky").forEach((sticky) => {
+  let rect = null;
+
+  sticky.addEventListener(
+    "mouseenter",
+    ({ target }) => {
+      isCursorLocked = true;
+
+      rect = target.getBoundingClientRect();
+
+      cursor.classList.add("is-locked");
+      cursor.style.setProperty("--top", rect.top + rect.height / 2 + "px");
+      cursor.style.setProperty("--left", rect.left + rect.width / 2 + "px");
+      cursor.style.setProperty("--width", rect.width + "px");
+      cursor.style.setProperty("--height", rect.height + "px");
+
+      target.style.setProperty("--scale", 1.05);
+    },
+    { passive: true }
+  );
+
+  sticky.addEventListener(
+    "mousemove",
+    ({ target }) => {
+      const halfHeight = rect.height / 2;
+      const topOffset = (event.y - rect.top - halfHeight) / halfHeight;
+      const halfWidth = rect.width / 2;
+      const leftOffset = (event.x - rect.left - halfWidth) / halfWidth;
+
+      cursor.style.setProperty("--translateX", `${leftOffset * 3}px`);
+      cursor.style.setProperty("--translateY", `${topOffset * 3}px`);
+
+      target.style.setProperty("--translateX", `${leftOffset * 6}px`);
+      target.style.setProperty("--translateY", `${topOffset * 4}px`);
+    },
+    { passive: true }
+  );
+
+  sticky.addEventListener(
+    "mouseleave",
+    ({ target }) => {
+      isCursorLocked = false;
+
+      cursor.style.setProperty("--width", DEFAULT_CURSOR_SIZE);
+      cursor.style.setProperty("--height", DEFAULT_CURSOR_SIZE);
+      cursor.style.setProperty("--translateX", 0);
+      cursor.style.setProperty("--translateY", 0);
+
+      target.style.setProperty("--translateX", 0);
+      target.style.setProperty("--translateY", 0);
+      target.style.setProperty("--scale", 1);
+
+      setTimeout(() => {
+        if (!isCursorLocked) {
+          cursor.classList.remove("is-locked");
+        }
+      }, 100);
     },
     { passive: true }
   );
