5cb5b68e0108a17d8f207a6b02ba5eae425aa15f
Thor <thorlindberg@icloud.com>
2021-04-19 16:37:26 +0200

    EmojIT: Added framework for styling of individual memoji elements

--- emojit/scripts.js
+++ emojit/scripts.js
@@ -1,3 +1,20 @@
+// character data structure
+
+window.character = {
+    "skin" : "template",
+    "hair" : "template",
+    "eyes" : "template"
+}
+
+// select a style option
+
+window.selected = "hair";
+function select(option) {
+    document.getElementById(window.selected).classList.remove("select");
+    window.selected = option;
+    document.getElementById(window.selected).classList.add("select");
+}
+
 // the link to your model provided by Teachable Machine export panel https://teachablemachine.withgoogle.com/
 const URL = "https://teachablemachine.withgoogle.com/models/5por9wdbQ/";
 
@@ -17,6 +34,10 @@ async function identify() {
     await webcam.play();
     window.requestAnimationFrame(loop);
 
+    // hide the start button, and show the style options
+    document.getElementById("start").style.display = "none";
+    document.getElementById("options").style.display = "flex";
+
 }
 
 async function loop() {
@@ -31,11 +52,14 @@ async function predict() {
     // predict can take in an image, video or canvas html element
     const prediction = await model.predict(webcam.canvas);
 
-    if (prediction[0].probability > 0.9) {
-        document.getElementById("character").src = "characters/blonde.png"
-    } else if (prediction[1].probability > 0.9) {
-        document.getElementById("character").src = "characters/brunette.png"
+    if (prediction[0].probability > 0.5) {
+        window.character["hair"] = "blonde";
+    } else if (prediction[1].probability > 0.5) {
+        window.character["hair"] = "brown";
     } else {
-        document.getElementById("character").src = "characters/ghost.png"
+        window.character["hair"] = "template";
     }
+
+    document.getElementById("character").src = "characters/" + window.character["skin"] + "_" + window.character["hair"] + "_" + window.character["eyes"] + ".png"
+
 }
