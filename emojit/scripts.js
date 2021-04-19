84e9df686b47628c8fe8360f270745e13eede3b1
Thor <thorlindberg@icloud.com>
2021-04-19 15:41:39 +0200

    Added EmojiIT

---
+++ emojit/scripts.js
@@ -0,0 +1,41 @@
+// the link to your model provided by Teachable Machine export panel https://teachablemachine.withgoogle.com/
+const URL = "https://teachablemachine.withgoogle.com/models/36P5oCMKu/";  //YOU NEED TO REPLACE THIS LINK
+
+let model, webcam;
+
+async function identify() {
+
+    // load the model and metadata
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";              
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
+
+    // setup a camera
+    webcam = new tmImage.Webcam(400, 400, true); // width, height, flip
+    await webcam.setup(); // request access to the webcam
+    await webcam.play();
+    window.requestAnimationFrame(loop);
+
+}
+
+async function loop() {
+    webcam.update(); // update the webcam frame
+    await predict();
+    window.requestAnimationFrame(loop);
+}
+
+// run the webcam image through the ML model
+async function predict() {
+    
+    // predict can take in an image, video or canvas html element
+    const prediction = await model.predict(webcam.canvas);
+
+    if (prediction[0].probability > 0.95) {
+        document.getElementById("character").src = "characters/blonde.png"
+    } else if (prediction[1].probability > 0.95) {
+        document.getElementById("character").src = "characters/brunette.png"
+    } else {
+        document.getElementById("character").src = "characters/ghost.png"
+    }
+}
