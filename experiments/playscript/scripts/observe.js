830ffd50235be8f8c9a29eb73f7a8702b9c2070a
Thor <thorlindberg@icloud.com>
2021-04-21 17:05:24 +0200

    Update observe.js

--- experiments/playscript/scripts/observe.js
+++ experiments/playscript/scripts/observe.js
@@ -2,9 +2,6 @@
 
 function request() {
 
-    window.lastExecution;
-
-    window.removeEventListener("devicemotion", pass);
     window.addEventListener("devicemotion", pass);
 
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
@@ -80,11 +77,6 @@ function pass(event) {
     // change Play button
     document.getElementById("request").innerHTML = "Ready!";
     document.getElementById("request").style.opacity = "0.3";
-    
-    // delay updates 300ms to avoid flickering numbers
-    var now = Date.now();
-    if (now - window.lastExecution < 300) return;
-    window.lastExecution = now;
 
     // get absolute value of acceleration parameters
     var x = Math.abs(event.acceleration.x);
@@ -93,27 +85,38 @@ function pass(event) {
 
     if (x > window.character["force"] || y > window.character["force"] || z > window.character["force"]) {
 
-        let players = []
-        if (window.characters["1"]["alive"]) { players.push(1) }
-        if (window.characters["2"]["alive"]) { players.push(2) }
-        if (window.characters["3"]["alive"]) { players.push(3) }
-        if (window.characters["4"]["alive"]) { players.push(4) }
-        
-        var next = window.characters["receiving"];
-        while (next == window.characters["receiving"]) {
-            let random = Math.floor(Math.random() * 4);
-            next = players[random];
+        if (window.characters["receiving"] == window.character["controller"]) {
+    
+            let players = []
+            if (window.characters["1"]["alive"]) { players.push(1) }
+            if (window.characters["2"]["alive"]) { players.push(2) }
+            if (window.characters["3"]["alive"]) { players.push(3) }
+            if (window.characters["4"]["alive"]) { players.push(4) }
+            
+            var next = window.characters["receiving"];
+            while (next == window.characters["receiving"]) {
+                let random = Math.floor(Math.random() * 4);
+                next = players[random];
+            }
+    
+            window.characters["receiving"] = next;
+
+            // update personal character styling
+            window.characters[window.character["controller"]]["color"] = window.character["color"];
+            window.characters[window.character["controller"]]["group"] = window.character["group"];
+
+            console.log("Passed ball!");
+
+            sendMessage(JSON.stringify(window.characters));
+
+            /*
+            let passing = new Date();
+            setTimeout(function() {
+                sendMessage(JSON.stringify(window.characters));
+            }, Math.min(3000, Math.abs(passing.getTime() - window.received.getTime())));
+            */
+
         }
-
-        window.characters["receiving"] = next;
-
-        // update personal character styling
-        window.characters[window.character["controller"]]["color"] = window.character["color"];
-        window.characters[window.character["controller"]]["group"] = window.character["group"];
-
-        console.log("Passed ball!");
-
-        sendMessage(JSON.stringify(window.characters));
 
     }
 
