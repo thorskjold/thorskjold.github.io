d8924a3cf12944edd1f6ed9e85481a7dc734fe0e
Thor <thorlindberg@icloud.com>
2021-04-25 22:11:51 +0200

    PlayScript: added window animation

--- experiments/playscript/scripts/toggles.js
+++ experiments/playscript/scripts/toggles.js
@@ -11,7 +11,10 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 function start() {
 
     // hide the start window
-    document.getElementById("window").style.display = "none";
+    document.getElementById("window").style.animationName = "hide"
+    setTimeout(function() {
+        document.getElementById("window").style.display = "none"
+    }, 250);
 
     // define players alive
     let players = ["1", "2", "3", "4"];
@@ -33,8 +36,11 @@ function restart() {
     document.getElementById("winner").pause();
     document.getElementById("winner").currentTime = 0;
 
-    // hide the final window
-    document.getElementById("final").style.display = "none";
+    // hide the start window
+    document.getElementById("final").style.animationName = "hide"
+    setTimeout(function() {
+        document.getElementById("final").style.display = "none"
+    }, 250);
 
     // reset data structure
     structure();
