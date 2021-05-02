6e77bfdb17d475d2c89164a0fd7d48c84f2acd78
Thor <thorlindberg@icloud.com>
2021-05-02 17:10:30 +0200

    Update gestures.js

--- experiments/playscript/scripts/gestures.js
+++ experiments/playscript/scripts/gestures.js
@@ -47,42 +47,42 @@ function swipe() {
     var xDown = null;                                                        
     var yDown = null;
 
-    function getTouches(evt) {
-        return evt.touches || // browser API
-        evt.originalEvent.touches; // jQuery
-    }                                                     
+}
 
-    function handleTouchStart(evt) {
-        const firstTouch = getTouches(evt)[0];                                      
-        xDown = firstTouch.clientX;                                      
-        yDown = firstTouch.clientY;                                      
-    }                                                
+function getTouches(evt) {
+    return evt.touches || // browser API
+    evt.originalEvent.touches; // jQuery
+}                                                     
 
-    function handleTouchMove(evt) {
-        if ( ! xDown || ! yDown ) { return }
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+}                                                
 
-        var xUp = evt.touches[0].clientX;                                    
-        var yUp = evt.touches[0].clientY;
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) { return }
 
-        var xDiff = xDown - xUp;
-        var yDiff = yDown - yUp;
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
 
-        if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) { /*most significant*/
-            if ( xDiff > 0 ) {
-                /* left swipe */ 
-            } else {
-                /* right swipe */
-            }                       
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) { /*most significant*/
+        if ( xDiff > 0 ) {
+            /* left swipe */ 
         } else {
-            if ( yDiff > 0 ) {
-                send()
-            } else { 
-                /* down swipe */
-            }                                                                 
-        }
-        /* reset values */
-        xDown = null;
-        yDown = null;                                             
+            /* right swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            send()
+        } else { 
+            /* down swipe */
+        }                                                                 
     }
-
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
 }
